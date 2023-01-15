b83f881f07c2e076e88c182f93f66ebc79ebcd96
Harry Vallon Jr <harryvallonjr@ymail.com>
2023-01-15 14:15:37 -0500

    commit tree fc055ee9a52cfaa3b04d3f00ceb355513e0eb2c9

--- app.rb
+++ app.rb
@@ -1,23 +1,8 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
-
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
-
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
-
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
+puts "Hello World"
 end
 
-puts add(1,3) + add(2,2)
+print "Pass this test, please"
+end
+
+p [1,2,3]
+end
